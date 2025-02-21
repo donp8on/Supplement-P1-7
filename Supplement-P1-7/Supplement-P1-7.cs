a1eed0b9646b62d9632c5f4d1c127a11fb9990f5
donp8on <paytonadonis@gmail.com>
2025-02-20 19:47:11 -0500

    chore: declared the three methods

--- Supplement-P1-7/Supplement-P1-7.cs
+++ Supplement-P1-7/Supplement-P1-7.cs
@@ -4,6 +4,21 @@ namespace Supplement_P1_7
 {
     public class Supplement_P1_7
     {
+        public int[] ReturnEvenNumbers(int skip = 0)
+        {
+            throw new NotImplementedException();
+        }
 
+
+        public int[] ShuffleAndReturnOddNumbers(int skip = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public (double average, int min, int max) ReturnAvgMinMax()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
