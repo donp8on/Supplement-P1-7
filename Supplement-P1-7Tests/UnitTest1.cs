62d14d8c899fb25eab7b7ab3bb93296cd5d8e95b
donp8on <paytonadonis@gmail.com>
2025-02-20 19:49:16 -0500

    test: created test for ReturnEvenNumbers

--- Supplement-P1-7Tests/UnitTest1.cs
+++ Supplement-P1-7Tests/UnitTest1.cs
@@ -2,10 +2,17 @@ namespace Supplement_P1_7Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Supplement_P1_7.Supplement_P1_7 operations = new();
 
+        [Fact]
+        public void ShouldReturnEvenNumbers_WithSkip()
+        {
+            int skipCount = 10;
+            var result = operations.ReturnEvenNumbers(skipCount);
+
+            Assert.All(result, num => Assert.True(num % 2 == 0));
+
+            Assert.Equal(skipCount * 2, result.First());
         }
     }
 }
