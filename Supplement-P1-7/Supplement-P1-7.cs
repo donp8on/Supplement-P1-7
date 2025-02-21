c935af8191da5a0e8287aade4345a3f4366c886b
donp8on <paytonadonis@gmail.com>
2025-02-20 19:56:23 -0500

    doc: documentated ReturnEvenNumbers method

--- Supplement-P1-7/Supplement-P1-7.cs
+++ Supplement-P1-7/Supplement-P1-7.cs
@@ -4,6 +4,11 @@ namespace Supplement_P1_7
 {
     public class Supplement_P1_7
     {
+        /// <summary>
+        /// An array of even numbers from 0 to 1,000,000 that are initialized to index.
+        /// </summary>
+        /// <param name="skip">This is the optional skip parameter.</param>
+        /// <returns>Returns an array of even numbers with the option to skip numbers in between.</returns>
         public int[] ReturnEvenNumbers(int skip = 0)
         {
             int[] array1 = Enumerable.Range(0, 1_000_000).ToArray();
